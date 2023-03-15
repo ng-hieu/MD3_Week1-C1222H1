ae46704781ecb4d48ebf9819d15a1965f7a62b39
Your Name <you@example.com>
2023-03-15 09:19:21 +0700

    TH1

---
+++ WBD1/TH1/app.js
@@ -0,0 +1,13 @@
+// const fs = require ('fs')
+// console.log("Start");
+// fs.readFile('test.txt', 'utf8', function (err, data) {
+//     console.log(data);
+// })
+// console.log("End")
+const fs = require('fs');
+
+console.log("Start");
+fs.readFile('test.txt','utf-8', function(err, data) {
+    console.log(data);
+});
+console.log("End");
